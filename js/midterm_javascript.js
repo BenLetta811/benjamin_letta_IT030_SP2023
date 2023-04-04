0eff6c269a287faf1cc4860c2a9dd3288d094193
BenLetta811 <124316331+BenLetta811@users.noreply.github.com>
2023-04-04 15:06:28 -0400

    Update midterm_javascript.js

--- js/midterm_javascript.js
+++ js/midterm_javascript.js
@@ -1,10 +1,10 @@
-// Get the current time
+// Get the current time with newDate
 var now = new Date();
 
-// Get the current hour
+// Get the current hour with getHours
 var hour = now.getHours();
 
-// Set the greeting message based on the current hour
+// Set the greeting message based on the current hour with morning, afternonn, evening, and nifht
 let greeting = '';
 if (hour < 12) {
   greeting = 'Good morning';
@@ -16,12 +16,13 @@ if (hour < 12) {
   greeting = 'Good night';
 }
 
-// Display the greeting message on the page
+// Display the greeting message on the page using 'greeting'
 var greetingElement = document.getElementById('greeting');
 if (greetingElement) {
   greetingElement.textContent = greeting;
 }
 /*-----------------------------------------------*/
+//Now create a var to get the width of the window when chaning page
 function checkViewportWidth() {
   var width = window.innerWidth;
   var footer = document.querySelector("footer");
